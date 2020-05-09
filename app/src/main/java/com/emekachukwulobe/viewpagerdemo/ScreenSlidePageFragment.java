e618c17bb4e8dbdda79dd36ee75d924d59258244
Emeka Chukwulobe <emekabe@yahoo.com>
2020-05-09 17:38:37 +0100

    View and fragment

---
+++ app/src/main/java/com/emekachukwulobe/viewpagerdemo/ScreenSlidePageFragment.java
@@ -0,0 +1,17 @@
+package com.emekachukwulobe.viewpagerdemo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return (ViewGroup) inflater.inflate(
+                R.layout.fragment_screen_slide_page, container, false);
+    }
+}
