660d563421e918e554aff9833c7f7fb2e760570d
Emeka Chukwulobe <emekabe@yahoo.com>
2020-05-10 16:09:03 +0100

    Uhmmmmm

--- app/src/main/java/com/emekachukwulobe/viewpagerdemo/MainActivity.java
+++ app/src/main/java/com/emekachukwulobe/viewpagerdemo/MainActivity.java
@@ -1,18 +1,17 @@
 package com.emekachukwulobe.viewpagerdemo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends FragmentActivity {
 
@@ -44,7 +43,15 @@ public class MainActivity extends FragmentActivity {
         pagerAdapter = new ScreenSlidePagerAdapter(this);
         viewPager.setAdapter(pagerAdapter);
 
+
+        String girls[] = {"Mia", "Grace", "Precious", "Zee", "Chidinma"};
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+//        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> tab.setText("OBJECT " +(position +1))).attach();
+//        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> tab.setText(position == 2 ? "Yam": "Obj")).attach();
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> tab.setText(girls[position])).attach();
     }
+
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/emekachukwulobe/viewpagerdemo/RoastedYamPageFragment.java
+++ app/src/main/java/com/emekachukwulobe/viewpagerdemo/RoastedYamPageFragment.java
@@ -8,7 +8,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class RoastedYamPageFragment extends Fragment {
 
     private Button agreeButton;
